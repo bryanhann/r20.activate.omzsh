68c32ba55b6db6df771f5b505ad16a0c280d9aec
Bryan Hann <nobody@nowhere>
2020-04-23 14:50:40 +0800

    Add theme

--- activate.sh
+++ activate.sh
@@ -1,8 +1,11 @@
 _export ZSH $(dirname $(grealpath $0))/__omzsh__
-
 [ -d $ZSH ] || {
     echo INSTALLING OMZSH
     $(dirname $(grealpath $0))/vendor/install.sh --unattended
 }
-
+#ZSH_THEME="robbyrussell"
+#ZSH_THEME="gnzh"
+#ZSH_THEME="mira"
+ZSH_THEME="bira"
+plugins=(git)
 source $ZSH/oh-my-zsh.sh
